808955c9b93f581f366a9498d43688e90931139d
Evan Weaver <evan.weaver@nrel.gov>
2016-01-26 10:14:22 -0700

    Initial analysis page commit.

--- app/app/analysis/analysisController.js
+++ app/app/analysis/analysisController.js
@@ -5,6 +5,20 @@ export class AnalysisController {
 
     this.test = 'Analysis Controller';
 
+    $scope.oneAtATime = true;
+
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addItem = function() {
+      var newItemNo = this.items.length + 1;
+      this.items.push('Item ' + newItemNo);
+    };
+
+    $scope.status = {
+      isFirstOpen: true,
+      isFirstDisabled: false
+    };
+
   }
 
 }
