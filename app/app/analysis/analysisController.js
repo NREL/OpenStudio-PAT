71f00e69441c52321452c09ebc1841b496047830
Evan Weaver <evan.weaver@nrel.gov>
2016-02-02 17:06:39 -0700

    Parsing object.

--- app/app/analysis/analysisController.js
+++ app/app/analysis/analysisController.js
@@ -13,6 +13,23 @@ export class AnalysisController {
 
     this.oneAtATime = true;
 
+    this.groups = [
+      {
+        title: 'Dynamic Group Header - 1',
+        content: 'Dynamic Group Body - 1'
+      },
+      {
+        title: 'Dynamic Group Header - 2',
+        content: ['Dynamic Group Body - 2']
+      }
+    ];
+
+    this.measurestuff = [
+      {
+        measurename: 'my test measure'
+      }
+    ];
+
     this.items = ['Item 1', 'Item 2', 'Item 3'];
 
     this.addItem = function () {
@@ -26,30 +43,49 @@ export class AnalysisController {
     };
 
     this.getMeasures();
-
-    this.$log.debug(parseString);
   }
 
   getMeasures() {
-    this.$log.debug('INSIDE getMeasures');
-    var self = this;
+    //this.$log.debug('INSIDE getMeasures');
+    //var self = this;
     this.$log.debug(this.srcDir.cwd());
     this.$log.debug(this.srcDir.find('.', {matching: '*/measure.xml'}, 'relativePath'));
-// use this.srcDir.read, or jetpack.read to get the file contents, then
-// use parseString to output JSON
-// put the resultant JSON into an accordian
-// move to the next file, make another accordian, and populate it
-
-    this.srcDir
-      .findAsync('.', {matching: '*/measure.xml'})
-      .then(function (measures) {
-        this.$log.debug('Async result', measures);
-        this._.each(measures, function (measure) {
-          console.log('hello');
-          self.$log.debug(measure);
-        });
-      }, function (msg) {
-        this.$log.debug('FAILED', msg);
+    var measures = this.srcDir.find('.', {matching: '*/measure.xml'}, 'relativePath');
+    for (var i = 0; i < measures.length; i++) {
+      //this.$log.debug('INSIDE measures for loop');
+      this.$log.debug(measures[i]);
+      //this.$log.debug(this.srcDir.read(measures[i]));
+      var measure = this.srcDir.read(measures[i]);
+      parseString(measure, function (err, result) {
+        //this.$log.debug(result);
+        var tag = result.measure.tags[0].tag[0]; // var tags = _.result(result, 'measure.tags[0].tag', []);
+        var attributes = result.measure.attributes[0].attribute;
+        console.log(result.measure.name[0]);
+        console.log(result.measure.uid[0]);
+        console.log(result.measure.version_id[0]);
+        console.log(result.measure.description[0]);
+        console.log(result.measure.modeler_description[0]);
+        console.log(result.measure.provenances[0]);
+        console.log(tag);
+        //console.log(attributes);
+        for (var j = 0; j < attributes.length; j++) {
+          console.log(attributes[j].name[0]);
+          console.log(attributes[j].value[0]);
+          console.log(attributes[j].datatype[0]);
+        }
       });
+    }
+
+    //this.srcDir
+    //  .findAsync('.', {matching: '*/measure.xml'})
+    //  .then(function (measures) {
+    //    this.$log.debug('Async result', measures);
+    //    this._.each(measures, function (measure) {
+    //      console.log('hello');
+    //      self.$log.debug(measure);
+    //    });
+    //  }, function (msg) {
+    //    this.$log.debug('FAILED', msg);
+    //  });
   }
 }
