2b7d93820d5c971edfae80dbb7f0fe76412137e6
Alex Swindler <Alex.Swindler@nrel.gov>
2016-02-16 15:49:58 -0700

    Added html file watcher

--- tasks/watch.js
+++ tasks/watch.js
@@ -7,4 +7,5 @@ var conf = require('./conf');
 gulp.task('watch', function () {
   gulp.watch(path.join(conf.paths.src, '/app/**/*.scss'), ['styles']);
   gulp.watch(path.join(conf.paths.src, '/app/**/*.js'), ['scripts:watch']);
+  gulp.watch(path.join(conf.paths.src, '/app/**/*.html'), ['partials']);
 });
