b779275e586561ed74785023a47c5e8905796383
Katherine Fleming <katherine.fleming@nrel.gov>
2016-04-13 16:00:03 -0600

    run tab

--- app/app/run/runController.js
+++ app/app/run/runController.js
@@ -9,9 +9,13 @@ export class RunController {
     vm.Project = Project;
     vm.runTypes = ['Run Locally', 'Run on Cloud'];
     vm.runTypes = vm.Project.getRunTypes();
-    vm.$scope.runType = vm.Project.getRunType();
 
+    vm.$scope.runType = vm.Project.getRunType();
     vm.$scope.selectedAnalysisType = vm.Project.getAnalysisType();
+    vm.$scope.disabledButtons = false;
+
+    // TODO: refresh this
+    vm.$scope.progress_amt = 66;
 
   }
 
@@ -20,6 +24,42 @@ export class RunController {
     vm.Project.setRunType(vm.runType);
   }
 
+  runEntireWorkflow() {
+    const vm = this;
+    vm.toggleButtons();
+  }
+
+  runSelected() {
+    const vm = this;
+    vm.toggleButtons();
+  }
+
+  runUpdated() {
+    const vm = this;
+    vm.toggleButtons();
+  }
+
+  runEnergyPlus() {
+    const vm = this;
+    vm.toggleButtons();
+  }
+
+  runReportingMeasures() {
+    const vm = this;
+    vm.toggleButtons();
+  }
+
+  toggleButtons() {
+    const vm = this;
+    vm.$scope.disabledButtons = !vm.$scope.disabledButtons;
+
+  }
+
+  cancelRun() {
+    const vm = this;
+    vm.toggleButtons();
+
+  }
 
 
 }
