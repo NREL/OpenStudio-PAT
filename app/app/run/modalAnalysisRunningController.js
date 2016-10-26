f3c2eade4c9f54651174250eae09f89319645b71
kflemin <katherine.fleming@nrel.gov>
2016-10-25 20:19:59 -0600

    run controller updates

--- app/app/run/modalAnalysisRunningController.js
+++ app/app/run/modalAnalysisRunningController.js
@@ -1,12 +1,14 @@
 export class ModalAnalysisRunningController {
 
-  constructor($log, $uibModalInstance) {
+  constructor($log, $uibModalInstance, OsServer) {
     'ngInject';
 
     const vm = this;
+    vm.OsServer = OsServer;
     vm.$uibModalInstance = $uibModalInstance;
     vm.$log = $log;
     vm.$log.debug('in Modal Analysis Controller');
+    vm.$log.debug('Analysis Status: ', vm.OsServer.getAnalysisStatus());
 
   }
 
