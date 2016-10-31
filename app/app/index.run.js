552b9927d2f0083ddb5fd0bfcaf389766d53e068
Evan Weaver <evan.weaver@nrel.gov>
2016-10-31 15:16:13 -0600

    Add mac menu items

--- app/app/index.run.js
+++ app/app/index.run.js
@@ -203,13 +203,37 @@ export function runBlock($rootScope, $state, $window, $document, $translate, Mea
       }, {
         type: 'separator'
       }, {
-        label: 'Quit',
-        accelerator: 'Command+Q',
+        label: 'New',
+        accelerator: 'Command+N',
         click() {
-          app.quit();
+          SetProject.newProject();
+        }
+      }, {
+        label: 'Open',
+        accelerator: 'Command+O',
+        click() {
+          SetProject.openProject();
+        }
+      }, {
+        label: 'Save',
+        accelerator: 'Command+S',
+        click() {
+          SetProject.saveProject();
+        }
+      }, {
+      label: 'Save As',
+        click() {
+        SetProject.saveAsProject();
+      }
+    }, {
+      label: 'Quit',
+      accelerator  : 'Command+Q',
+        click()    {
+        app.quit();
         }
       }]
-    });
+      });
+
     // Window menu.
     template[3].submenu.push({
       type: 'separator'
