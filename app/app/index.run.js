66d9dc8a73f27fbfee8754944823afae019b0e81
kflemin <katherine.fleming@nrel.gov>
2016-11-28 20:45:06 -0700

    remove undo and redo from electron edit menu

--- app/app/index.run.js
+++ app/app/index.run.js
@@ -117,16 +117,6 @@ export function runBlock($rootScope, $state, $window, $document, $translate, toa
   const template = [{
     label: 'Edit',
     submenu: [{
-      label: 'Undo',
-      accelerator: 'CmdOrCtrl+Z',
-      role: 'undo'
-    }, {
-      label: 'Redo',
-      accelerator: 'Shift+CmdOrCtrl+Z',
-      role: 'redo'
-    }, {
-      type: 'separator'
-    }, {
       label: 'Cut',
       accelerator: 'CmdOrCtrl+X',
       role: 'cut'
