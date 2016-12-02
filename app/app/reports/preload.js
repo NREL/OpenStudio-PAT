3f6cc0ac41bd47ab33319409b6035d254a8d87f7
Kyle Benne <kyle.benne@nrel.gov>
2016-12-02 08:52:39 -0600

    Tell linter to ignore preload error

--- app/app/reports/preload.js
+++ app/app/reports/preload.js
@@ -3,6 +3,8 @@ console.info('Loaded preload.js');
 
 // note that adding var or let before this fixes the
 // linter error but breaks the functionality.
+/*eslint-disable */
 js2xmlparser = require('js2xmlparser');
+/*eslint-enable */
 
 
