5062523b55a6263b8d6ade1404adf923a51ccda9
ljbrackney <larry.brackney@nrel.gov>
2017-04-22 08:50:23 -0700

    Changed nature of Office Study project slightly to have a ZNE aspect that will be of interest to users

---
+++ sample_projects/Office_Study/measures/ffca943f-bb8f-4b9d-8bf8-24d1e8cb1c18/tests/add_rooftop_pv_test.rb
@@ -0,0 +1,65 @@
+require 'openstudio'
+require 'openstudio/ruleset/ShowRunnerOutput'
+require 'minitest/autorun'
+require_relative '../measure.rb'
+require 'fileutils'
+
+class AddRooftopPVTest < MiniTest::Unit::TestCase
+
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_good_argument_values
+    # create an instance of the measure
+    measure = AddRooftopPV.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Ruleset::OSRunner.new
+
+    # load the test model
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/pv_test_input.osm")
+    model = translator.loadModel(path)
+    assert((not model.empty?))
+    model = model.get
+
+    # get arguments
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
+
+    # create hash of argument values.
+    # If the argument has a default that you want to use, you don't need it in the hash
+    args_hash = {}
+    # using defaults values from measure.rb for other arguments
+
+    # populate argument with specified hash value if specified
+    arguments.each do |arg|
+      temp_arg_var = arg.clone
+      if args_hash[arg.name]
+        assert(temp_arg_var.setValue(args_hash[arg.name]))
+      end
+      argument_map[arg.name] = temp_arg_var
+    end
+
+    # run the measure
+    measure.run(model, runner, argument_map)
+    result = runner.result
+
+    # save the model to test output directory
+    output_file_path = OpenStudio::Path.new(File.dirname(__FILE__) + "/output/test_output.osm")
+    model.save(output_file_path,true)
+
+    # show the output
+    #show_output(result)
+
+    # assert that it ran correctly
+    assert_equal("Success", result.value.valueName)
+    # assert(result.info.size == 1)
+    # assert(result.warnings.size == 0)
+
+  end
+
+end
