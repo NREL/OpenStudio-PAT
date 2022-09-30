8c3f2d1943efb7508b7a5ffc61c06c110d37f22c
Bailey Stein <bailey.stein@deptagency.com>
2022-09-30 16:13:15 -0400

    #230: Add DeleteResultsModalPO

---
+++ playwright/page-objects/modals/delete-results-modal.po.ts
@@ -0,0 +1,20 @@
+import { ModalPO } from './modal.po';
+
+export class DeleteResultsModalPO extends ModalPO {
+  static readonly EXPECTED_TITLE = 'Delete Local Results?';
+  static readonly EXPECTED_FOOTER_BUTTONS = {
+    OK: 'OK',
+    CANCEL: 'Cancel'
+  };
+  static readonly EXPECTED_BODY_TEXT = {
+    newRun: 'Running a new analysis will delete your local results. Are you sure you want to continue?',
+    newRunType: 'Selecting a new run type will delete your local results.  Are you sure you want to continue?',
+    newRunOnSelected:
+      'Running a new analysis will delete your local results for the selected datapoints.  Are you sure you want to continue?'
+  };
+
+  static async isOk() {
+    await super.isOk();
+    await this.isBodyTextOk(this.EXPECTED_BODY_TEXT.newRun);
+  }
+}
--- playwright/page-objects/modals/index.ts
+++ playwright/page-objects/modals/index.ts
@@ -1,3 +1,4 @@
+export * from './delete-results-modal.po';
 export * from './modal.po';
 export * from './new-project-modal.po';
 export * from './project-modal.po';
