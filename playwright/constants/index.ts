e4ea1c71068f0c63ae628acb2bc281235f149689
Bailey Stein <bailey.stein@deptagency.com>
2022-10-07 10:53:02 -0400

    #230: Add PO and tests for Reports page

--- playwright/constants/index.ts
+++ playwright/constants/index.ts
@@ -4,4 +4,5 @@ export * from './expected-details-by-page';
 export * from './expected-menu-items';
 export * from './expected-outputs-by-project';
 export * from './projects';
+export * from './report-view';
 export * from './toast-levels';
