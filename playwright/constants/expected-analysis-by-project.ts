2bce7e48c38fe6aa82bee90f3b1f63382123fbca
Bailey Stein <bailey.stein@deptagency.com>
2022-10-08 16:37:48 -0400

    #230: Add PO and tests for Analysis page

--- playwright/constants/expected-analysis-by-project.ts
+++ playwright/constants/expected-analysis-by-project.ts
@@ -5,14 +5,20 @@ export enum AnalysisType {
   ALGORITHMIC = 'Algorithmic'
 }
 
+export enum MeasureTypes {
+  OPEN_STUDIO = 'OpenStudio Measures',
+  ENERGY_PLUS = 'EnergyPlus Measures',
+  REPORTING = 'Reporting Measures'
+}
+
 export interface AnalysisDetails {
   type: AnalysisType;
   algorithmicMethod?: string;
   defaultSeedModel?: string;
   defaultWeatherFile?: string;
-  openStudioMeasures: string[];
-  energyPlusMeasures: string[];
-  reportingMeasures: string[];
+  [MeasureTypes.OPEN_STUDIO]: string[];
+  [MeasureTypes.ENERGY_PLUS]: string[];
+  [MeasureTypes.REPORTING]: string[];
 }
 
 export const EXPECTED_ANALYSIS_BY_PROJECT: Record<Projects, AnalysisDetails> = {
@@ -20,28 +26,35 @@ export const EXPECTED_ANALYSIS_BY_PROJECT: Record<Projects, AnalysisDetails> = {
     type: AnalysisType.MANUAL,
     defaultSeedModel: 'SmallOffProto.osm',
     defaultWeatherFile: 'USA_CO_Golden-NREL.724666_TMY3.epw',
-    openStudioMeasures: [
+    [MeasureTypes.OPEN_STUDIO]: [
       'AedgOfficeHvacFanCoilDoas',
       'AedgOfficeHvacVavDx',
       'AedgOfficeHvacRadiantDoas',
       'AedgOfficeHvacVavChW'
     ],
-    energyPlusMeasures: ['XcelEDATariffSelectionandModelSetup'],
-    reportingMeasures: ['OpenStudio Results']
+    [MeasureTypes.ENERGY_PLUS]: ['XcelEDATariffSelectionandModelSetup'],
+    [MeasureTypes.REPORTING]: ['OpenStudio Results']
   },
   [Projects.OFFICE_STUDY]: {
     type: AnalysisType.ALGORITHMIC,
-    algorithmicMethod: 'Design of Experiments (DOE)',
+    algorithmicMethod: 'Design Of Experiments (DOE)',
     defaultSeedModel: 'empty.osm',
     defaultWeatherFile: 'USA_CO_Golden-NREL.724666_TMY3.epw',
-    openStudioMeasures: [
+    [MeasureTypes.OPEN_STUDIO]: [
       'Create DOE Prototype Building',
       'Reduce Lighting Loads by Percentage',
       'Reduce Electric Equipment Loads by Percentage',
       'Add Rooftop PV'
     ],
-    energyPlusMeasures: ['XcelEDATariffSelectionandModelSetup'],
-    reportingMeasures: ['OpenStudio Results']
+    [MeasureTypes.ENERGY_PLUS]: ['XcelEDATariffSelectionandModelSetup'],
+    [MeasureTypes.REPORTING]: ['OpenStudio Results']
   },
-  [Projects.NEW]: { type: AnalysisType.MANUAL, openStudioMeasures: [], energyPlusMeasures: [], reportingMeasures: [] }
+  [Projects.NEW]: {
+    type: AnalysisType.MANUAL,
+    defaultSeedModel: '',
+    defaultWeatherFile: '',
+    [MeasureTypes.OPEN_STUDIO]: [],
+    [MeasureTypes.ENERGY_PLUS]: [],
+    [MeasureTypes.REPORTING]: []
+  }
 };
